838c3d67ab2c11130948bb5af2c97b8e6adfaad9
Shiwen An <sweynan@icloud.com>
2022-05-27 15:16:40 +0900

    Dataset added for testing

--- util.py
+++ util.py
@@ -20,9 +20,52 @@ import sklearn
 from sklearn import svm
 from sklearn.datasets import fetch_openml, make_moons, make_circles
 from sklearn.model_selection import train_test_split
+import pandas as pd
 
 from IPython.display import clear_output
 from tqdm import tqdm
+
+NQUBIT = 3 # For current testing
+NEVT = 500
+
+def data():
+  # class label, lepton 1 pT, lepton 1 eta, lepton 1 phi, lepton 2 pT, lepton 2 eta, lepton 2 phi, missing energy magnitude, missing energy phi, MET_rel, axial MET, M_R, M_TR_2, R, MT2, S_R, M_Delta_R, dPhi_r_b, cos(theta_r1)
+  df = pd.read_csv("Files/HIGGS_1K.csv",names=('isSignal',
+      'lep1_pt','lep1_eta','lep1_phi',
+      'lep2_pt','lep2_eta','lep2_phi',
+      'miss_ene','miss_phi','MET_rel',
+      'axial_MET','M_R','M_TR_2','R',
+      'MT2','S_R',
+      'M_Delta_R','dPhi_r_b','cos_theta_r1'))
+  feature_dim = NQUBIT   # dimension of each data point
+  if feature_dim == 3:
+      SelectedFeatures = ['lep1_pt', 'lep2_pt', 'miss_ene']
+  elif feature_dim == 5:
+      SelectedFeatures = ['lep1_pt','lep2_pt','miss_ene','M_TR_2','M_Delta_R']
+  elif feature_dim == 7:
+      SelectedFeatures = ['lep1_pt','lep1_eta','lep2_pt','lep2_eta','miss_ene','M_TR_2','M_Delta_R']
+
+  #jobn = JOBN
+  training_size = NEVT
+  testing_size = NEVT
+  #shots = 1024
+  #uin_depth = NDEPTH_UIN
+  #uvar_depth = NDEPTH_UVAR
+  #niter = NITER
+  #backend_name = 'BACKENDNAME'
+  #option = 'OPTION'
+  #random_seed = 10598+1010*uin_depth+101*uvar_depth+jobn
+
+  # Train Test split
+  df_sig = df.loc[df.isSignal==1, SelectedFeatures]
+  df_bkg = df.loc[df.isSignal==0, SelectedFeatures]
+
+  df_sig_training = df_sig.values[:training_size]
+  df_bkg_training = df_bkg.values[:training_size]
+  df_sig_test = df_sig.values[training_size:training_size+testing_size]
+  df_bkg_test = df_bkg.values[training_size:training_size+testing_size]
+  training_input = {'1':df_sig_training, '0':df_bkg_training}
+  test_input = {'1':df_sig_test, '0':df_bkg_test}
 
 
 class QKM:
@@ -55,5 +98,6 @@ def example():
 if __name__ == '__main__' :
   # call the function 
   # directly
+  data()
   test()
   example()
